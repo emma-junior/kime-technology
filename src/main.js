631dd2f5d2cfedf13db89cc1a9e01fe9b72c8da3
Emmanuel <emnzboy31@gmail.com>
2025-05-07 19:41:35 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import "./style.css";
+import gsap from "gsap";
+
+export default class Home {
+  constructor() {
+    this.homeIntro();
+  }
+
+  homeIntro() {
+    let tl = gsap.timeline();
+    function init() {
+      tl.from("#app", { ease: "linear", autoAlpha: 0 })
+        .fromTo(
+          ".logo",
+          {
+            clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)",
+          },
+          {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            duration: 1.5,
+            ease: "power2.out",
+          }
+        )
+        .to(".logo", {
+          ease: "linear",
+          scale: 0.6,
+          duration: 1,
+          transform: "translate(45vw,0)",
+        });
+    }
+    window.addEventListener("load", function (event) {
+      init(); //do stuff
+      console.log("Home Intro");
+    });
+  }
+}
+
+new Home();
